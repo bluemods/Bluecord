ec2835618cd765fd6c5f1db6e264925371983692
Blue <blue@bluesmods.com>
2025-09-20 11:15:41 -0400

    Fix guild joining

---
+++ app/src/main/java/mods/parser/GuildJoinFix.kt
@@ -0,0 +1,26 @@
+package mods.parser
+
+import com.discord.api.guild.GuildFeature
+import com.discord.models.domain.ModelInvite
+import com.discord.models.guild.Guild
+import mods.utils.LogUtils
+
+object GuildJoinFix {
+
+    private val TAG = GuildJoinFix::class.java.simpleName
+
+    @JvmStatic
+    fun hasVerificationGate(guild: Guild?, modelInvite: ModelInvite?): Boolean {
+        LogUtils.log(TAG, "I'm here")
+
+        val features = guild?.features.orEmpty().ifEmpty { modelInvite?.guild?.features }.orEmpty()
+        val hasGate = if (GuildFeature.MEMBER_VERIFICATION_GATE_ENABLED in features) {
+            GuildFeature.COMMUNITY in features || GuildFeature.MEMBER_VERIFICATION_MANUAL_APPROVAL in features
+        } else {
+            false
+        }
+
+        LogUtils.log(TAG, "hasGate=$hasGate, guild=$guild")
+        return hasGate
+    }
+}
