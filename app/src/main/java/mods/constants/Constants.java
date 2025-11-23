94ea8fd6fc32732037098c4ef87b3cf3356a6d94
Blue <blue@bluesmods.com>
2025-11-23 18:03:09 -0500

    2.7.4

--- app/src/main/java/mods/constants/Constants.java
+++ app/src/main/java/mods/constants/Constants.java
@@ -11,8 +11,8 @@ public final class Constants {
 
     // Version number. This is changed on release
     // and is used for checking if Bluecord needs an update
-    public static final int    VERSION_CODE = 27300;
-    public static final String VERSION_NAME = "2.7.3";
+    public static final int    VERSION_CODE = 27400;
+    public static final String VERSION_NAME = "2.7.4";
 
     // Adds support for Bluecord emotes and other systems that use a '.'
     @Language("Regexp")
